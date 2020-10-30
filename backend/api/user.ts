a19478da18b173c565dac827c4b5336fb81a4b32
Takeshi <iwatakeshi@gmail.com>
2020-10-30 14:22:57 -0600

    WIP Refactor
    * Replace AWS Lambda with Fastify
    * Replace Typeorm with Prisma

--- backend/api/user.ts
+++ backend/api/user.ts
@@ -5,61 +5,66 @@ import * as jwt from 'jsonwebtoken';
 import * as cookie from 'cookie';
 
 import { Database } from '../db'
-import { User } from '../entities/User';
+
+import fp from 'fastify-plugin';
+import { FastifyPluginCallback } from 'fastify';
+import client from '../prisma/client';
+import { User } from '@prisma/client';
 
 const saltRounds = 10;
 const SECRET_KEY = process.env.SECRET_KEY || 'mysecret';
 const ERROR_RESPONSES = {
-  MISSING_CREDENTIALS: { statusCode: 401, body: JSON.stringify({ message: 'Please include username and password' })},
-  INVALID_TOKEN: { statusCode: 401, body: JSON.stringify({ message: 'invalid token' })},
-  INVALID_CREDENTIALS: { statusCode: 401, body: JSON.stringify({ message: 'Please include username and password' })},
-  INVALID_SESSION: { statusCode: 401, body: JSON.stringify({ message: 'session no longer valid'})},
+  MISSING_CREDENTIALS: { statusCode: 401, body: JSON.stringify({ message: 'Please include username and password' }) },
+  INVALID_TOKEN: { statusCode: 401, body: JSON.stringify({ message: 'invalid token' }) },
+  INVALID_CREDENTIALS: { statusCode: 401, body: JSON.stringify({ message: 'Please include username and password' }) },
+  INVALID_SESSION: { statusCode: 401, body: JSON.stringify({ message: 'session no longer valid' }) },
 }
-const generateAccessToken = (user: User) =>  jwt.sign({ id: user.id, access: true }, SECRET_KEY, { expiresIn: '15m' });
+const generateAccessToken = (user: User) => jwt.sign({ id: user.id, access: true }, SECRET_KEY, { expiresIn: '15m' });
 const generateRefreshToken = (user: User) => jwt.sign({ id: user.id, refresh: true }, SECRET_KEY, { expiresIn: '7d' });
 
-module.exports.signup = async (event: APIGatewayProxyEvent, context: Context) => {
-  context.callbackWaitsForEmptyEventLoop = false
+module.exports = fp(async function users(fastify, _opts, next) {
 
-  if (!event.body) return ERROR_RESPONSES.MISSING_CREDENTIALS;
+  fastify.post('/signup', async function signup(req) {
+    if (!req.body) return ERROR_RESPONSES.MISSING_CREDENTIALS
 
-  const { username, password } = JSON.parse(event.body);
-  if (!username || !password) return ERROR_RESPONSES.MISSING_CREDENTIALS;
+    try {
+      const { username, password } = JSON.parse(req.body as string)
 
-  const database = new Database();
+      const passwordHash = await bcrypt.hash(password, saltRounds);
 
-  let db: Connection = await database.getConnection();
+      const user = await client.user.create({
+        data: { username, password: passwordHash }, select: {
+          username: true,
+          password: false,
+          token: false,
+        }
+      })
 
-  const passwordHash = await bcrypt.hash(password, saltRounds);
+      const accessToken = generateAccessToken(user as User);
+      const refreshToken = generateRefreshToken(user as User);
 
-  try {
-    const userRepo = db.getRepository(User);
-    const user = await userRepo.save({ username, passwordHash });
-
-    const accessToken = generateAccessToken(user);
-    const refreshToken = generateRefreshToken(user);
-
-    await userRepo.update(user, { refreshToken });
-
-    delete user.passwordHash;
-    delete user.refreshToken;
-
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-        'Set-Cookie': cookie.serialize('refreshToken', refreshToken, {
-          httpOnly: true,
-          maxAge: 60 * 60 * 24 * 7, // 7 days
-        }),
-      },
-      body: JSON.stringify({ user, accessToken }),
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+          'Set-Cookie': cookie.serialize('refreshToken', refreshToken, {
+            httpOnly: true,
+            maxAge: 60 * 60 * 24 * 7, // 7 days
+          }),
+        },
+        body: JSON.stringify({ user, accessToken }),
+      }
+    } catch (error) {
+      return { statusCode: 409, body: JSON.stringify({ message: e.message }) };
     }
-  } catch (e) {
-    return { statusCode: 409, body: JSON.stringify({ message: e.message }) };
-  }
-}
+
+  })
+
+  fastify.post('/login',)
+
+  next()
+} as FastifyPluginCallback)
 
 module.exports.login = async (event: APIGatewayProxyEvent, context: Context) => {
   context.callbackWaitsForEmptyEventLoop = false;
@@ -87,7 +92,7 @@ module.exports.login = async (event: APIGatewayProxyEvent, context: Context) => 
 
     if (!passwordMatch) throw 'Invalid username/password combination';
 
-    return { 
+    return {
       statusCode: 200,
       headers: {
         'Access-Control-Allow-Origin': '*',
@@ -119,9 +124,9 @@ module.exports.refresh = async (event: APIGatewayProxyEvent, context: Context) =
   let db: Connection = await database.getConnection();
 
   try {
-    const decoded = await new Promise((resolve, reject) => 
-      jwt.verify(refreshToken, SECRET_KEY, async function(err, decoded) { if (err) reject(err); resolve(decoded) }));
-    
+    const decoded = await new Promise((resolve, reject) =>
+      jwt.verify(refreshToken, SECRET_KEY, async function (err, decoded) { if (err) reject(err); resolve(decoded) }));
+
     const { id, refresh } = decoded as { id: number, refresh?: boolean };
 
     if (!refresh || !id) return ERROR_RESPONSES.INVALID_SESSION;
@@ -169,8 +174,8 @@ module.exports.me = async (event: APIGatewayProxyEvent, context: Context) => {
   let db: Connection = await database.getConnection();
 
   try {
-    const decoded = await new Promise((resolve, reject) => 
-      jwt.verify(accessToken, SECRET_KEY, async function(err, decoded) { if (err) reject(err); resolve(decoded) }));
+    const decoded = await new Promise((resolve, reject) =>
+      jwt.verify(accessToken, SECRET_KEY, async function (err, decoded) { if (err) reject(err); resolve(decoded) }));
 
     const { id, access } = decoded as { id: number, access?: boolean };
 
@@ -187,7 +192,7 @@ module.exports.me = async (event: APIGatewayProxyEvent, context: Context) => {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true,
       },
-      body: JSON.stringify({user}),
+      body: JSON.stringify({ user }),
     }
   } catch (e) {
     return ERROR_RESPONSES.INVALID_TOKEN;
@@ -210,13 +215,13 @@ module.exports.logout = async (event: APIGatewayProxyEvent, context: Context) =>
   let db: Connection = await database.getConnection();
 
   try {
-    const decoded = await new Promise((resolve, reject) => 
-      jwt.verify(refreshToken, SECRET_KEY, async function(err, decoded) { if (err) reject(err); resolve(decoded) }));
-    
+    const decoded = await new Promise((resolve, reject) =>
+      jwt.verify(refreshToken, SECRET_KEY, async function (err, decoded) { if (err) reject(err); resolve(decoded) }));
+
     const { id, refresh } = decoded as { id: number, refresh?: boolean };
 
     if (!refresh || !id) return ERROR_RESPONSES.INVALID_SESSION;
-  
+
     const userRepo = await db.getRepository(User);
 
     await userRepo.update(id, { refreshToken: '' });
